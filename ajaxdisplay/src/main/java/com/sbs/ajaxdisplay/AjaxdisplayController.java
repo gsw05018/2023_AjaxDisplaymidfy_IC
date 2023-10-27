7eaf242f9c3010d49fa56d9d5b5ac44a65b27665
gsw05018 <gsw05018@naver.com>
2023-10-27 21:59:01 +0900

    ajax endpoint 셋팅

--- ajaxdisplay/src/main/java/com/sbs/ajaxdisplay/AjaxdisplayController.java
+++ ajaxdisplay/src/main/java/com/sbs/ajaxdisplay/AjaxdisplayController.java
@@ -1,14 +1,45 @@
 package com.sbs.ajaxdisplay;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AjaxdisplayController {
+	private int count1 = 0;
+	private int count2 = 0;
 
 	@GetMapping("/main1")
-	public String showMain(){
+	public String showMain(Model model){
+
+		model.addAttribute("count1", count1);
+		model.addAttribute("count2", count2);
+
 		return "main1";
 	}
+	@GetMapping("/count1")
+	@ResponseBody
+	public RsData<Integer> getCount1(Model model) {
+		return new RsData<>("S-1", "current count1", count1);
+	}
 
+	@GetMapping("/count2")
+	@ResponseBody
+	public RsData<Integer> getCount2(Model model) {
+		return new RsData<>("S-1", "current count2", count2);
+	}
+
+	@GetMapping("/increaseCount1")
+	@ResponseBody
+	public RsData<Integer> increaseCount1(Model model) {
+		return new RsData<>("S-1", "count1 increased", ++count1);
+	}
+
+	@GetMapping("/increaseCount2")
+	@ResponseBody
+	public RsData<Integer> increaseCount2(Model model) {
+		return new RsData<>("S-1", "count2 increased", ++count2);
+	}
 }
+
